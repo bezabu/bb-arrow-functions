1a4b0850c35f7989800dd1ad1d1e22e926406e22
bezabu <>
2023-12-12 15:07:11 +0000

    Arrow functions and spread operators

--- arrow-functions.js
+++ arrow-functions.js
@@ -2,13 +2,42 @@
  * To run this file in Gitpod, use the 
  * command node arrow-functions.js in the terminal
  */
-
+/*
 // Vanilla JavaScript Function
-
+function addTwoNumbers(num1, num2) {
+    return num1 + num2
+}
+let sum = addTwoNumbers(3,5,);
+console.log(sum);
+*/
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+    return a + b
+}
+let sum = addTwoNumbers(3,5,);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(3,5,);
+console.log(sum2);
+
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello there!!');
 
+const sayHello = () => console.log('hello');
+sayHello();
+
+/*
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    '<p>
+        This is a multiline string!
+    </p>'
+)
+console.log(returnMultipleLines());
+*/
+
+
